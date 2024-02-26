b57d27337c6c7a124f01a8045eb4ec239d46e6e3
Karthikeyan.B <bkarthi1000@gmail.com>
2024-02-26 17:37:05 +0530

    updates  in authService

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,41 +1,71 @@
 import { Injectable, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { GoogleAuthProvider } from '@angular/fire/auth';
+import { GoogleAuthProvider, user } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  userData:any;
+  userData: any;
 
   constructor(
     private firebaseAuthenticationService: AngularFireAuth,
     private router: Router,
     private ngZone: NgZone
   ) {
-      this.firebaseAuthenticationService.authState.subscribe((user)=>{
-        if(user){
-          this.userData = user;
-          localStorage.setItem('user',JSON.stringify(this.userData));
-        }else{
-          localStorage.setItem('user','null');
-        }
-      })
-   }
-
-   logInWithGoogleProvider(){
-    return this.firebaseAuthenticationService.signInWithPopup(new GoogleAuthProvider())
-            .then(()=> this.observeUserState())
-            .catch((error:Error)=>{
-              alert(error.message);
-            })
-   }
-
-   observeUserState(){
-    this.firebaseAuthenticationService.authState.subscribe((userState)=>{
-      userState && this.ngZone.run(()=>this.router.navigate(['home']));
+    this.firebaseAuthenticationService.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+      } else {
+        localStorage.setItem('user', 'null');
+      }
     })
-   }
+  }
+
+  logInWithGoogleProvider() {
+    return this.firebaseAuthenticationService.signInWithPopup(new GoogleAuthProvider())
+      .then(() => this.observeUserState())
+      .catch((error: Error) => {
+        alert(error.message);
+      })
+  }
+
+  loginWithEmailAndPassword(email: string, password: string) {
+    this.firebaseAuthenticationService.signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        this.userData = userCredential.user;
+        this.observeUserState()
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+  }
+
+  signUpWithEmailAndPassword(email: string, password: string) {
+    return this.firebaseAuthenticationService.createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        this.userData = userCredential.user;
+        this.observeUserState()
+      })
+  }
+
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    return user !== null;
+  }
+  
+  logOut() {
+    return this.firebaseAuthenticationService.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['login']);
+    })
+  }
+  observeUserState() {
+    this.firebaseAuthenticationService.authState.subscribe((userState) => {
+      userState && this.ngZone.run(() => this.router.navigate(['home']));
+    })
+  }
 }
