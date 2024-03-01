90be07c42410432e71cf608480cb22d00ea7ff7e
Karthikeyan.B <bkarthi1000@gmail.com>
2024-03-01 20:02:11 +0530

    shops ui update

--- src/app/user/shop/shop.component.ts
+++ src/app/user/shop/shop.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Shop } from 'src/app/shared/interfaces/shop.interface';
 import { FetchService } from 'src/app/shared/services/fetch.service';
 
 @Component({
@@ -11,17 +12,58 @@ import { FetchService } from 'src/app/shared/services/fetch.service';
 export class ShopComponent implements OnInit {
   id: string = '';
   type: string = '';
+  panelOpenState = false;
+  
+  product: Shop = {
+    name: '',
+    description: '',
+    addressLane1: '',
+    addressLane2: '',
+    city: '',
+    rating: 0,
+    type: 'product',
+    avgPricing: '',
+    category: '',
+    subcategory: '',
+    products: []
+  };
+
 
   constructor(private route: ActivatedRoute, private fet: FetchService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') || '';
     this.fetchType();
+    this.fetchShop();
   }
 
   fetchType() {
     return this.fet.fetchType(this.id).subscribe((res) => {
       this.type = res;
     })
+  }
+
+  fetchShop() {
+    this.fet.fetchShop(this.id).subscribe((res) => {
+      if (res) {
+        this.product = res;
+      }
+    })
+  }
+
+  getStars(rating: number): string[] {
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 !== 0;
+    const stars: string[] = [];
+
+    for (let i = 0; i < fullStars; i++) {
+      stars.push('star');
+    }
+
+    if (halfStar) {
+      stars.push('star_half');
+    }
+
+    return stars;
   }
 }
