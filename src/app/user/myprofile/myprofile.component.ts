573c20dfa235e490fa3cbec7c29451ba9ccb39ac
Karthikeyan.B <bkarthi1000@gmail.com>
2024-03-10 00:18:29 +0530

    profile page ui

--- src/app/user/myprofile/myprofile.component.ts
+++ src/app/user/myprofile/myprofile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FetchService } from 'src/app/shared/services/fetch.service';
+import { UserProfile } from 'src/app/shared/interfaces/userProfile.interface';
 
 @Component({
   selector: 'app-myprofile',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./myprofile.component.css']
 })
 export class MyprofileComponent {
-
+firstName: any;
+register() {
+throw new Error('Method not implemented.');
+}
+  uid!: any;
+  user!: any;
+  constructor(private fet: FetchService) {
+    this.uid = this.fet.getUserId().subscribe((res) => {
+      this.uid = res;
+      this.fet.getUserInfo(res).subscribe((user) => {
+        this.user = user;
+      }
+      )
+    });
+  }
 }
